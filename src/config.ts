ce8216ba70f4edd8c8147a4e8befa75869afccb3
sfdfgvdvdsvb <106359677+sfdfgvdvdsvb@users.noreply.github.com>
2023-05-19 20:51:25 -0700

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  //Site display name
+  SITE_TITLE: "SkriptMovies",
+
+  //Put your discord link here or null to disable
+  SITE_DISCORD: "",
+  
+  //True or false to show or hide site credit in footer (You should leave enabled 😉)
+  SITE_CREDITS: false,
+
+  //Do not change or site will break!
+  RIPPER_API: "https://api.ripper.fun"
+}
