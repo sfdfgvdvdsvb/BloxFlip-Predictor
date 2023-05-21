720aff9df01fd4ab77b97c8a8b448c5c143503d3
sfdfgvdvdsvb <106359677+sfdfgvdvdsvb@users.noreply.github.com>
2023-05-20 18:05:34 -0700

    Update index-414795d7.js

--- public/assets/index-414795d7.js
+++ public/assets/index-414795d7.js
@@ -10371,8 +10371,10 @@ function sm(e) {
     return [l, i]
 }
 const le = {
-    SITE_TITLE: "SkriptMovies.online - Watch movies and Shows online free in Full HD on SkriptMovies",
-    SITE_TELEEGRAM: "https://t.me/SkriptMovies",
+    SITE_TITLE: "Fossa.tv - Watch movies and Shows online free in Full HD on Fossa",
+    SITE_TELEEGRAM: "https://t.me/fossatv",
+    SITE_DISCORD: "https://discord.gg/XkwehvjGyA",
+    SITE_STATUS: "https://status.fossa.tv/status/status",
     SITE_CREDITS: !0,
     SITE_SNOWFLAKES: 10,
     RIPPER_API: "https://api.ripper.fun"
@@ -10380,21 +10382,30 @@ const le = {
 function cm() {
     return L("footer", {
         children: [v("img", {
-            src: "/assets/logo.png",
+            src: "/assets/logo2.png",
             alt: "Logo"
         }), L("div", {
+            className: "links",
+            children: [v("a", {
+                href: le.SITE_STATUS,
+                target: "_blank",
+                children: "Status"
             }), v("a", {
                 href: le.SITE_TELEEGRAM,
                 target: "_blank",
                 children: "Telegram"
             }), v("a", {
-                href: "https://skriptmovies.online",
+                href: le.SITE_DISCORD,
                 target: "_blank",
-                children: "❤️ by Skript"
+                children: "Discord"
+            }), v("a", {
+                href: "https://fossa.tv",
+                target: "_blank",
+                children: "❤️ by Ripper"
             })]
         }), v("p", {
             className: "disclaimer",
-            children: "2023 © SkriptMovies.online | We do not store any media."
+            children: "2023 © Fossa.tv | We do not store any media."
         })]
     })
 }
@@ -11530,7 +11541,7 @@ function ov() {
             children: v("div", {
                 className: "home-alert",
                 children: v("p", {
-                    children: "Ads allow us to keep SkriptMovies online. Support us by sharing SkriptMovies with friends! ❤️"
+                    children: "Ads allow us to keep Fossa online. Support us by sharing Fossa with friends! ❤️"
                 })
             })
         }), v("div", {
