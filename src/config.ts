28657328a14e5ff25161afd77edd28c282d062dd
sfdfgvdvdsvb <106359677+sfdfgvdvdsvb@users.noreply.github.com>
2023-11-23 23:29:51 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default {
   SITE_TITLE: "SkriptMovies",
 
   //Put your discord link here or null to disable
-  SITE_DISCORD: "https://t.me/+BuEzoycCZoUzYWRh",
+  SITE_DISCORD: "",
   
   //True or false to show or hide site credit in footer (You should leave enabled 😉)
   SITE_CREDITS: true,
